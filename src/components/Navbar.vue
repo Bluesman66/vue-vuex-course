397b262f1cb8fe46f806da804ef5cb8537b4d82b
Alex Paklin <alexander.pak@prpl.global>
2021-12-12 15:54:24 +0500

    navbar, router, links

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar color="orange" dark>
+    <v-spacer />
+    <v-btn outlined class="ml-3">
+      <router-link to="/">Main</router-link>
+    </v-btn>
+    <v-btn outlined class="ml-3">
+      <router-link to="/photo">Photo</router-link>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
