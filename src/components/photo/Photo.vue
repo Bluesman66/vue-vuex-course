9c9720916404244eb77cdc3635ab53fc23036bce
Alex Paklin <alexander.pak@prpl.global>
2021-12-12 16:24:12 +0500

    fake photos list

---
+++ src/components/photo/Photo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ photo.id + " " + photo.title }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    photo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style  scoped>
+</style>
--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>PHOTOS</div>
+  <v-container
+    ><Photo v-bind:key="photo.id" v-for="photo in photos" v-bind:photo="photo"
+  /></v-container>
 </template>
 
 <script>
-export default {};
+import Photo from "../components/photo/Photo";
+export default {
+  components: { Photo },
+  data: () => ({
+    photos: [
+      { id: 1, title: "photo 1" },
+      { id: 2, title: "photo 2" },
+      { id: 3, title: "photo 3" },
+      { id: 4, title: "photo 4" },
+    ],
+  }),
+};
 </script>
 
 <style  scoped>
