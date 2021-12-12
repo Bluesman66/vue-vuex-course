1e6fd14f4adcf9c35fd599d5339b1975e1f9026f
Alex Paklin <alexander.pak@prpl.global>
2021-12-12 16:49:38 +0500

    real photos list

--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,21 +1,35 @@
 <template>
-  <v-container
-    ><Photo v-bind:key="photo.id" v-for="photo in photos" v-bind:photo="photo"
-  /></v-container>
+  <v-container>
+    <v-row>
+      <Photo
+        :key="photo.id"
+        v-for="photo in photos"
+        :photo="photo"
+      />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import Photo from "../components/photo/Photo";
 export default {
   components: { Photo },
+
   data: () => ({
-    photos: [
-      { id: 1, title: "photo 1" },
-      { id: 2, title: "photo 2" },
-      { id: 3, title: "photo 3" },
-      { id: 4, title: "photo 4" },
-    ],
+    photos: [],
   }),
+
+  methods: {
+    fetchTodo() {
+      this.axios
+        .get("https://jsonplaceholder.typicode.com/photos?_limit=10")
+        .then((response) => (this.photos = response.data));
+    },
+  },
+
+  mounted() {
+    this.fetchTodo();
+  },
 };
 </script>
 
